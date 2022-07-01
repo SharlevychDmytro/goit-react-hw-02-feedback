233a97c7f31eee6be909b53d2ba3ea605acfe741
Sharlevych Dmytro <sharlevychdmytro@gmail.com>
2022-07-02 02:01:41 +0300

    add markup not style

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,60 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import { Component } from 'react';
+import { Box } from 'components/Box';
+import { Section } from 'components/Section/Section';
+import { Statistics } from 'components/Statistics/Statistics';
+import { FeedbackOptions } from 'components/FeedbackOptions/FeedbackOptions';
+
+export class App extends Component {
+  state = {
+    good: 0,
+    neutral: 0,
+    bad: 0,
+  };
+
+  stateKeys = Object.keys(this.state);
+
+  incrementStat = e => {
+    const currentBtn = e.currentTarget.textContent.toLowerCase();
+    this.setState(prState => ({ [currentBtn]: (prState[currentBtn] += 1) }));
+  };
+
+  countTotalFeedback = () => {
+    return Object.values(this.state).reduce((acc, tot) => acc + tot);
+  };
+
+  countPositiveFeedbackPercentage = () => {
+    return Math.floor((this.state.good / this.countTotalFeedback()) * 100);
+  };
+
+  render() {
+    return (
+      <Box bg="background">
+        <Section title="Please leave feedback">
+          <Box>
+            {this.stateKeys.map(stateKey => {
+              return (
+                <FeedbackOptions
+                  options={
+                    stateKey[0].toLocaleUpperCase() +
+                    stateKey.slice(1, stateKey.length)
+                  }
+                  key={stateKey}
+                  onLeaveFeedback={this.incrementStat}
+                />
+              );
+            })}
+          </Box>
+
+          <Statistics
+            title="Statistics"
+            good={this.state.good}
+            neutral={this.state.neutral}
+            bad={this.state.bad}
+            total={this.countTotalFeedback()}
+            positivePercentage={this.countPositiveFeedbackPercentage()}
+          />
+        </Section>
+      </Box>
+    );
+  }
+}
